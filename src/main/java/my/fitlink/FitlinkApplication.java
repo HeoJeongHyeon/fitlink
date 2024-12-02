8a504d83679fb24c7adc5725f1788fbd81a0b012
HeoJeongHyeon <jh7e0n20@gmail.com>
2024-12-02 19:48:03 +0900

    Initial commit

---
+++ src/main/java/my/fitlink/FitlinkApplication.java
@@ -0,0 +1,13 @@
+package my.fitlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitlinkApplication.class, args);
+    }
+
+}
